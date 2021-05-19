20192bc6fa43822b7620a0cae955ffbd14f419e4
Arnor Neto <arnorneto10@gmail.com>
2021-05-19 16:06:54 -0300

    add level and game pages to router

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { GlobalContext } from "./GlobalContext";
 import Landing from "./layout/Landing";
 import Home from "./layout/Home";
+import Level from "./layout/Level";
+import Game from "./layout/Game";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <GlobalContext>
           <Route exact path="/" component={Landing} />
           <Route exact path="/home" component={Home} />
+          <Route exact path="/level" component={Level} />
+          <Route exact path="/game" component={Game} />
         </GlobalContext>
       </Switch>
     </Router>
--- src/layout/Home/index.js
+++ src/layout/Home/index.js
@@ -1,14 +1,20 @@
 import * as S from "./styled";
 import { Context } from "../../GlobalContext";
 import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
+  let history = useHistory();
   const context = useContext(Context);
+
+  const toLevel = () => {
+    history.push("/level")
+  }
 
   return (
     <S.Wrapper>
       <h1>Olá, {context.nickname.get}</h1>
-      <button>Jogar</button>
+      <button onClick={toLevel}>Jogar</button>
       <button>Glossário</button>
       <button>Notícias</button>
       <a href="https://google.com">Sobre a ideia</a>
