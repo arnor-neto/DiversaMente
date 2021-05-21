176875aefc990df35647724559f86a5820cb5ff0
Arnor Neto <arnorneto10@gmail.com>
2021-05-21 18:51:26 -0300

    button style adjustments

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,8 @@
 import * as S from "./styled";
 
-const Button = ({ onClick, children, color = "primary" }) => {
+const Button = ({ onClick, children, color = "primary", size = "big" }) => {
   return (
-    <S.Button onClick={onClick} color={color}>
+    <S.Button onClick={onClick} color={color} size={size}>
       {children}
     </S.Button>
   );
--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const Button = styled.button`
   border: 1px solid #dcdcdc;
-  width: 50%;
-  font-size: 1.3rem;
+  width: ${props => props.size === "big"? "50%" : "48%"};
+  font-size: 1.2rem;
   padding: 0.5rem;
   border-radius: 4px;
-  margin-bottom: 4%;
+  margin-bottom: 3%;
   background-color: ${(props) =>
     props.color === "primary" ? "#DCDCDC" : "white"};
 `;
