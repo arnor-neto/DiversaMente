30dd65bf6f57d5bba4939218c5bceecc185d81dd
Arnor Neto <arnorneto10@gmail.com>
2021-05-24 11:31:41 -0300

    add difficulty to global context

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -4,18 +4,25 @@ export const Context = createContext();
 
 const InitialState = {
   nickname: localStorage.getItem("nickname"),
+  difficulty: sessionStorage.getItem("difficulty"),
 };
 
 export const GlobalContext = ({ children }) => {
   const [nickname, setNickname] = useState(InitialState.nickname);
+  const [difficulty, setDifficulty] = useState(InitialState.difficulty);
 
   const store = {
     nickname: { get: nickname, set: setNickname },
+    difficulty: { get: difficulty, set: setDifficulty },
   };
 
   useEffect(() => {
     localStorage.setItem("nickname", nickname);
   }, [nickname]);
+
+  useEffect(() => {
+    sessionStorage.setItem("difficulty", difficulty);
+  }, [difficulty]);
 
   return <Context.Provider value={store}>{children}</Context.Provider>;
 };
