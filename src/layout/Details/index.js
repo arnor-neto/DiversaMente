c5b06c198908ff3a6364ccd55b0d3edc41b41f10
Arnor Neto <arnorneto10@gmail.com>
2021-05-26 18:50:29 -0300

    update details page

--- src/layout/Details/index.js
+++ src/layout/Details/index.js
@@ -1,20 +1,32 @@
 import * as S from "./styled";
 import ReturnButton from "../../components/ReturnButton";
 import { Context } from "../../GlobalContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const Details = () => {
   const context = useContext(Context);
+  const [card, setCard] = useState();
+  let cardID = context.selectedCardID.get;
+
+  useEffect(() => {
+    axios
+      .get(`https://diversamente-api.herokuapp.com/api/cards/list/${cardID}`)
+      .then((response) => {
+        setCard(response.data);
+      });
+  }, []);
 
   return (
     <S.Wrapper>
       <S.Header>
         <ReturnButton />
-        <S.Title>genero id: {context.selectedCardID.get}</S.Title>
+        <S.Title>{card?.name}</S.Title>
         <div style={{ width: "60.3px" }} />
       </S.Header>
       <S.Content>
-        <p>Conteúdo</p>
+        <S.Flag src={card?.image}/>
+        <S.P>{card?.description}</S.P>
       </S.Content>
     </S.Wrapper>
   );
--- src/layout/Details/styled.js
+++ src/layout/Details/styled.js
@@ -5,7 +5,8 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   width: 100%;
-  height: 100vh;
+  background: linear-gradient(180deg, #c33764 0%, #1d2671 100%);
+  background-attachment: fixed;
 `;
 
 export const Header = styled.div`
@@ -26,13 +27,10 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 87vh;
+  min-height: 87vh;
   padding: 0 2% 2% 2%;
 
   @media (min-width: 900px) {
-    flex-wrap: wrap;
-    flex-direction: row;
-    justify-content: space-evenly;
     width: 90%;
   }
 `;
@@ -40,4 +38,26 @@ export const Content = styled.div`
 export const Title = styled.p`
   margin: 0;
   font-size: 1.3rem;
+  color: #F2F2F2;
+`;
+
+export const Flag = styled.img`
+  border-radius: 8px;
+  margin-bottom: 16px;
+  align-self: center;
+  
+  @media (min-width: 900px) {
+    width: 400px;
+  }
+`;
+
+export const P = styled.p`
+  color: #F2F2F2; 
+  text-align: center;
+  width: 90%;
+  align-self: center;
+  
+  @media (min-width: 900px) {
+    width: 70%;
+  }
 `;
